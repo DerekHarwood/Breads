01d6bd412ecc22436b13ef45a179847fd1555656
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-07-20 20:14:54 -0400

    Update breads_controller.js

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -19,6 +19,14 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 // SHOW
 // http://localhost:3003/breads/new
 breads.get('/:arrayIndex', (req, res) => {
@@ -50,6 +58,17 @@ breads.post('/', (req, res) => {
 breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 module.exports = breads
