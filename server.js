d1a8de4eb44c0f43605eed845134b6a21bd36598
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-07-29 19:52:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -32,6 +33,10 @@ app.get('*', (req, res) => {
 })
 
 // LISTEN
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
