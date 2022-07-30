f2b14ef838eb20fd22ac528d20f9c70963c64d98
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-07-30 13:51:39 -0400

    Update bakers_controller.js

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -2,12 +2,21 @@
 const express = require('express')
 const baker = express.Router()
 const Baker = require('../models/baker.js')
-const bakerSeedData = require('../models/baker_seed.js')
+const bakerSeedData = require('../models/baker_seed.js')                    
+
+// Index: 
+baker.get('/', (req, res) => {
+    Baker.find()
+        .populate('breads')
+        .then(foundBakers => {
+            res.send(foundBakers)
+        })
+})                    
 
 baker.get('/data/seed', (req, res) => {
     Baker.insertMany(bakerSeedData)
         .then(res.redirect('/breads'))
-})
+})                   
 
 // export
 module.exports = baker                    
